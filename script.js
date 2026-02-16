6cadab067462c49c61e671b03c529f29aa8cfb0c
Abdullrazak Hassan <151748410+samanesec@users.noreply.github.com>
2026-02-16 20:03:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ btn.addEventListener("click", () => {
   const attemptedLogin =
     user.value.trim().length > 0 || pass.value.trim().length > 0;
 
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F738RE1812"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F738RE1812');
+</script>
+
   // Umami event logging (kun adferd)
   if (window.umami) {
     umami.track(
