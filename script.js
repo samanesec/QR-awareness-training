c5fbcd75e7a1d62cc9dd7b33d308d11cd2b03086
Abdullrazak Hassan <151748410+samanesec@users.noreply.github.com>
2026-02-17 14:08:51 +0100

    Refactor login attempt redirection and logging

--- script.js
+++ script.js
@@ -6,22 +6,12 @@ btn.addEventListener("click", () => {
   const attemptedLogin =
     user.value.trim().length > 0 || pass.value.trim().length > 0;
 
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F738RE1812"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-F738RE1812');
-</script>
-
-  // Umami event logging (kun adferd)
+  // Umami event logging
   if (window.umami) {
     umami.track(
       attemptedLogin ? "login_attempt_input" : "login_attempt_no_input"
     );
   }
 
-  window.location.href = `reveal.html?attempt=${attemptedLogin ? "1" : "0"}`;
+  window.location.href = "reveal.html?attempt=" + (attemptedLogin ? "1" : "0");
 });
